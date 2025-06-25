56af63ba4ef2328f379061100439d7c096ac942b
night last <gulaliyevgulali923@gmail.com>
2025-06-25 09:39:20 +0300

    ESLint + Prettier added last modfy

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,31 +1,45 @@
-// .eslintrc.js
-export default {
-    root: true,
+// api/eslint.config.js
+import { FlatCompat } from '@eslint/eslintrc'
+import pluginPrettier from 'eslint-plugin-prettier'
+import pluginTs from '@typescript-eslint/eslint-plugin'
 
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        project: ['./tsconfig.json'],
-        tsconfigRootDir: __dirname,
-        ecmaVersion: 2020,
-        sourceType: 'module',
-    },
+// FlatCompat, legacy "plugin:@…" extends’lerini Flat-Config’e çevirir
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+})
 
-    plugins: [
-        '@typescript-eslint',
-        'prettier',
-    ],
-
-    extends: [
+export default [
+    // önce TS önerilenleri ve Prettier Recommended’ı alalım
+    ...compat.extends(
         'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
-    ],
-
-    rules: {
-        'prettier/prettier': ['error', {
-            singleQuote: true,
-            semi: false,
-            endOfLine: 'auto',
-        }],
-        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+        'plugin:prettier/recommended'
+    ),
+    {
+        // sonra plugin modüllerini obje olarak tanımlayın
+        plugins: {
+            '@typescript-eslint': pluginTs,
+            prettier: pluginPrettier,
+        },
+        // parser ve parserOptions da Flat-Config’te languageOptions içinde
+        languageOptions: {
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                project: ['./tsconfig.json'],
+                tsconfigRootDir: __dirname,
+                ecmaVersion: 2020,
+                sourceType: 'module',
+            },
+        },
+        rules: {
+            // Prettier hatalarını ESLint hatası olarak göster
+            'prettier/prettier': [
+                'error',
+                { singleQuote: true, semi: false, endOfLine: 'auto' },
+            ],
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                { argsIgnorePattern: '^_' },
+            ],
+        },
     },
-}
+]
