83eea734fad748b3fc5e4b3828de5d978f79715c
night last <gulaliyevgulali923@gmail.com>
2025-06-25 12:39:17 +0300

    chore: replace any with unknown in cache typings scene #6

--- api/src/types/cache-reset.d.ts
+++ api/src/types/cache-reset.d.ts
@@ -2,8 +2,8 @@ import 'cache-manager'
 
 declare module 'cache-manager' {
     interface Cache {
-        get<T = any>(key: string): Promise<T | undefined>
-        set<T = any>(key: string, value: T, ttl?: number): Promise<void>
+        get<T = unknown>(key: string): Promise<T | undefined>
+        set<T = unknown>(key: string, value: T, ttl?: number): Promise<void>
         del(key: string): Promise<void>
         reset(): Promise<void>
     }
