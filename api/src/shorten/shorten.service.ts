a42eb1e012e58d203f7f4d4e647a406b5c9a9dea
night last <gulaliyevgulali923@gmail.com>
2025-06-25 12:55:07 +0300

    fix: cache clearin with id  scene #9

--- api/src/shorten/shorten.service.ts
+++ api/src/shorten/shorten.service.ts
@@ -106,8 +106,10 @@ export class ShortenService {
         })
         if (!link) throw new NotFoundException('Link not found')
 
-        await this.prisma.click.deleteMany({ where: { linkId: link.id } })
-        await this.prisma.link.delete({ where: { shortCode } })
+        await this.prisma.$transaction([
+            this.prisma.click.deleteMany({ where: { linkId: link.id } }),
+            this.prisma.link.delete({ where: { id: link.id } }),
+        ])
 
         await Promise.all([
             this.cacheManager.del(`info:${shortCode}`),
@@ -117,7 +119,6 @@ export class ShortenService {
 
         return { deleted: true }
     }
-
 
     async analytics(shortCode: string) {
         const link = await this.prisma.link.findUnique({
